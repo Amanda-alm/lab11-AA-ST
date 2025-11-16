51d1b7d34446669b628d6873fef5711042874b13
Spencer Treadway <spencer.treadway08@gmail.com>
2025-11-16 14:14:54 -0500

    modified test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -9,11 +9,16 @@ Partner 2: Spencer Treadway
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):  # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(1, 0), 1)
+        self.assertEqual(add(1, -1), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self):  # 3 assertions
+        self.assertEqual(sub(1, 1), 0)
+        self.assertEqual(sub(1, -1), 2)
+        self.assertEqual(sub(3, 5), -2)
+
     # ##########################
 
     ######## Partner 1
@@ -24,23 +29,24 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide(self):  # 3 assertions
         # Note: This tests the function 'b / a'
-        self.assertEqual(divide(2, 10), 5.0)  # 10 / 2
-        self.assertAlmostEqual(divide(5, 2), 0.4)  # 2 / 5
-        self.assertAlmostEqual(divide(-4, 10), -2.5)
+        self.assertEqual(div(2, 10), 5.0)  # 10 / 2
+        self.assertAlmostEqual(div(5, 2), 0.4)  # 2 / 5
+        self.assertAlmostEqual(div(-4, 10), -2.5)
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(2, 8), 3)
+        self.assertEqual(log(10, 100), 2)
+        self.assertEqual(log(2, 0.5), -1)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
+        with self.assertRaises(ValueError):
+            log(1, 10)
     # ##########################
     
     ######## Partner 1
