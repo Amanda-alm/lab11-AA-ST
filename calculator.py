5b5838f1782d10ccf0816d169b9835c6aaf5baea
Amanda-alm <ama@orphic.me>
2025-11-16 12:56:22 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,19 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
 # First example
 def add(a, b): 
-    pass
+    a + b
+def subtract(a,b): a - b
+
+def multiply(a,b): a * b
+
+def divide(a,b): b /a
+
+def logarithm(a,b): loga(b) 
+
+def exponent(a,b): ab
 
 
