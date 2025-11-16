23bc432893a9e9a3a1c93c523cabf9e796e5c23b
Amanda-alm <ama@orphic.me>
2025-11-16 14:11:39 -0500

    moved import

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,6 @@
 #Partner 1: Amanda Almeida
 #Partner 2: Spencer Treadway
 
-"""
-https://github.com/Amanda-alm/lab11-AA-ST
-Partner 1: Amanda Almeida
-Partner 2: Spencer Treadway
-
-"""
 import unittest
 from calculator imp
 class TestCalculator(unittest.TestCase):
